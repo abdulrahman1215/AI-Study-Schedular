cf1ed9bc8c2cacfbc6c7b29f92c041fb1af59646
Arahman024 <mdabdulrahman6432248@gmail.com>
2026-05-17 08:21:53 +0530

    Setup React frontend architecture

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="text-4xl text-center mt-20">AI Study Scheduler</div>
+  )
+}
+
+export default App
---
+++ client/src/layouts/MainLayout.tsx
@@ -0,0 +1,7 @@
+const MainLayout = () => {
+    return (
+        <div className="min-h-screen bg-slate-900 text-white">Main Layout</div>
+    )
+}
+
+export default MainLayout
